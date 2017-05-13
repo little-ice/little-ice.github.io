d56fa82de18d02f71f98dbcfefa4881d20642efc
ice <727413621@qq.com>
2017-05-13 18:04:38 +0800

    最后更新详情页页面

--- js/details.js
+++ js/details.js
@@ -9,5 +9,19 @@ require(['../config'],function(){
 				$(".fixed-nav").hide();
 			}
 		});
+		$('.eat').click(function(){
+			$(this).parents('.other-title').find('.l').css({borderColor:"#bbb"});
+			$(this).parents('.other-title').find('.m').css({borderColor:"#599C3B"});
+			$(this).addClass("active").siblings().removeClass('active');
+			$(this).parents('.other-l').find('.content1').hide();
+			$(this).parents('.other-l').find('.content2').show();
+		});
+		$('.goods').click(function(){
+			$(this).parents('.other-title').find('.l').css({borderColor:"#599C3B"});
+			$(this).parents('.other-title').find('.m').css({borderColor:"#bbb"});
+			$(this).addClass("active").siblings().removeClass('active');
+			$(this).parents('.other-l').find('.content2').hide();
+			$(this).parents('.other-l').find('.content1').show();
+		});
 	});
 })
